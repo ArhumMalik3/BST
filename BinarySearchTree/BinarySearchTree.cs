2e8e579ae5476d0c1721d250feb3154d4f10f053
ArhumMalik3 <ArhumMalik95@gmail.com>
2020-09-02 10:59:39 -0500

    init

---
+++ BinarySearchTree/BinarySearchTree.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    class BinarySearchTree
+    {
+        public Node root;
+
+        public void Add(int currentNodeValue)
+        {
+
+            int currentNode; 
+
+            if (root == null)
+            {
+                root = new Node(currentNodeValue);
+            }
+
+            else
+            {
+                currentNode = currentNodeValue;
+            }
+            
+            if (root.Data > currentNodeValue)
+            {
+                root.leftNode.Data = currentNodeValue;
+            }
+
+            if (root.Data < currentNodeValue)
+            {
+                root.rightNode.data = currentNodeValue;
+            }
+
+        }
+
+
+        public void Search()
+        {
+
+        }
+    }
+}
